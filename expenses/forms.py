536fdb41bc30962feb3a06837dddb944eb7cddb3
trashykhoifish1 <ntran41@asu.edu>
2023-12-12 01:59:49 -0700

    Add an amount field

--- expenses/forms.py
+++ expenses/forms.py
@@ -5,14 +5,16 @@ from .models import Expense
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
-        fields = ['date_due', 'name', 'payer']
+        fields = ['date_due', 'name', 'amount', 'payer']
         labels = {
             'date_due': 'Date Due',
             'name': 'Expense Name',
+            'amount': 'Amount',
             'payer': 'Payer'
         }
         widgets = {
             'date_due': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'amount': forms.NumberInput(attrs={'class': 'form-control'}),
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'payer': forms.TextInput(attrs={'class': 'form-control'})
         }
--- expenses/models.py
+++ expenses/models.py
@@ -5,9 +5,10 @@ class Expense(models.Model):
     date_logged = models.DateField(auto_now_add=True)
     date_due = models.DateField()
     name = models.CharField(max_length=100)
+    amount = models.FloatField()
     payer = models.CharField(max_length=50)
     paid = models.BooleanField(default=False)
 
     def __str__(self) -> str:
-        return f'Expense: {self.name} - {self.date_due}'
+        return f'Expense: {self.name}: ${self.amount} - {self.date_due}'
 
