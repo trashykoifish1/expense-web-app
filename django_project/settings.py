78db1fa55780ea6f5d36524d9ca39b4202a7b4a0
trashykhoifish1 <ntran41@asu.edu>
2023-12-17 02:20:33 -0700

    Merge with running_app

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-67_koc1cj2*s&(ax5_yw9cu@@sqd=$-!8qx-@*ova39fz==8z-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.5", "127.0.0.1"]
 
 
 # Application definition
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join( BASE_DIR, "static/")
